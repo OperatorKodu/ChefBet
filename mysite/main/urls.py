13ca59a42414cc50b34f108d7581b1cdcbfd5f44
Jakub Baran <kubabaran2000@wp.pl>
2021-06-01 20:49:11 +0200

    Początek frontu

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('more/<int:event_id>/', views.more, name='more'),
+    path('my_coupons/', views.my_coupons, name='more'),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Category
 
 
 def index(request):
-    return HttpResponse({
-        "<h1>Hello World</h1>"
-    })
+    category = Category.name
+    template = loader.get_template('main/index.html')
+    context = {
+        'categories': category,
+    }
+    return HttpResponse(template.render(context, request))
 
+
+def more(request, event_id):
+    response = "You're looking at the more types of event %s."
+    return HttpResponse(response % event_id)
+
+
+def my_coupons(request):
+    response = "Here you can find your active and old coupons"
+    return HttpResponse(response)
